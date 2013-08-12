8ce809abc1845be4f99f932bbeb9c01fbd2e0b28
John Granström <granstrom.john@gmail.com>
2013-08-12 16:40:15 +0200

    Update example.js

--- example.js
+++ example.js
@@ -1,5 +1,12 @@
 var quack = require('./src/quack');
 
 quack
-  .ensure('string', [1, 2]).either(quack.isString, quack.isTrueArray).not(quack.isNull).exists()
+	.ensure('string', [1, 2]).either(quack.isString, quack.isTrueArray).not.isNull().exists()
 	.ensure(true).isBoolean()
+
+quack.ensure('string').not.isNull();
+quack.ensure('strinng').either(quack.not.isNull, quack.isNumber);
+
+
+quack.ensure('string').not.isNull().isString().either(quack.isNumber, quack.not.isNull);
+quack.ensure()
