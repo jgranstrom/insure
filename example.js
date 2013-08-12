68f9247573abc2130797cf3010c40818279f2105
John Granström <granstrom.john@gmail.com>
2013-08-12 17:16:22 +0200

    Update example.js

--- example.js
+++ example.js
@@ -1,11 +1,25 @@
 var quack = require('./src/quack');
 
 quack
-	.ensure('string', [1, 2]).either(quack.isString, quack.isTrueArray).not.isNull().exists()
+	.ensure('string', [1, 2]).either(quack.isString, quack.isTrueArray).and.not.isNull().and.exists()
 	.ensure(true).isBoolean()
 
 quack.ensure('string').not.isNull();
 quack.ensure('strinng').either(quack.not.isNull, quack.isNumber);
 
 
-quack.ensure('string').not.isNull().isString().either(quack.isNumber, quack.not.isNull);
+quack.ensure('string').not.isNull().and.isString().and.either(quack.isNumber, quack.not.isNull);
+
+
+function test(x, y, z) {
+	quack
+		.ensure(x).isNumber()
+		.ensure(y).not.isNull().and.isBoolean()
+		.ensure(z).either(quack.isString, quack.isBoolean);
+
+	console.log('arguments are all okay!');
+}
+
+test(3, true, 'hello'); // Okay
+test(3, true, false);   // Okay
+test(3, true, 10);      // error!
